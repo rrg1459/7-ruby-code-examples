adb64d7c831f64a8d3439c7e601a3a6cacbf2c3b
rafaDev <rrg1459@hotmail.com>
2024-01-19 11:20:33 -0400

    Add some comments

--- with_index_example.rb
+++ with_index_example.rb
@@ -1,3 +1,7 @@
+# Would you like to have the current index while iterating over a collection of items?
+
+# You can use the with_index method.
+
 def reverse_alternate(string)
   string.gsub(/[^\s]+/).with_index { |w, idx| idx.even? ? w : w.reverse }
 end
@@ -5,3 +9,8 @@ end
 str = 'Apples are some Good'
 puts str
 puts reverse_alternate(str) # "Apples era Good"
+
+# Notice:
+
+# We combine with_index & even? to find if we have to reverse the current word
+# Gsub without a block returns an Enumerator object, which allows you to chain it with other methods
