10ccd2fed58c650221c26bdbfe53aeb9b03489c3
rafaDev <rrg1459@hotmail.com>
2024-01-19 11:04:23 -0400

    Add comments and inspect

--- recursion_and_stack_example.rb
+++ recursion_and_stack_example.rb
@@ -1,3 +1,12 @@
+
+# Recursion is when a method calls itself multiple times as a way to make progress towards a solution.
+
+# Many interesting problems can be solved with recursion.
+
+# But because recursion has its limits, you can use a stack data structure instead.
+
+# Let’s look at an example where we want to find out the “Power Set” of a given array. The Power Set is a set of all the subsets that can be created from the array.
+
 def get_numbers(list, index = 0, taken = [])
   return [taken] if index == list.size
 
@@ -20,6 +29,8 @@ end
 
 puts "-"*40
 puts get_numbers([1,2,3])
+puts get_numbers([1,2,3]).inspect
 puts "-"*40
 puts get_numbers_stack([1,2,3])
+puts get_numbers_stack([1,2,3]).inspect
 puts "-"*40
