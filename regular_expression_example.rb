1fe8ea0a01815dd1f80cae335a007811a704856e
rafaDev <rrg1459@hotmail.com>
2024-01-19 10:53:18 -0400

    Add comments and some examples

--- regular_expression_example.rb
+++ regular_expression_example.rb
@@ -1,3 +1,5 @@
+# We want to find out if a given string follows a pattern of VOWEL to NON-VOWEL characters.
+
 def alternating_characters?(s)
   type = [/[aeiou]/, /[^aeiou]/].cycle
   if s.start_with?(/[^aeiou]/)
@@ -5,4 +7,5 @@ def alternating_characters?(s)
   end
   s.chars.all? { |ch| ch.match?(type.next) }
 end
-puts alternating_characters?("ateciyanu")
+puts alternating_characters?("ateciyanu") # true
+puts alternating_characters?("ateciyau") # false
