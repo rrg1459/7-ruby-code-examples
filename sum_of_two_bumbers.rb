044348140a82d2d94d41595d8ab7e3cf20c6dac9
rafaDev <rrg1459@hotmail.com>
2024-01-19 10:16:44 -0400

    Add comments and some examples

--- sum_of_two_bumbers.rb
+++ sum_of_two_bumbers.rb
@@ -1,6 +1,10 @@
+# In this example we want to find out if given an array of unique numbers, there is a combination of two numbers which adds up to a target number.
+
 def sum_eq_n?(arr, n)
   return true if arr.empty? && n == 0
   arr.product(arr).reject { |a,b| a == b }.any? { |a,b| a + b == n }
 end
 
-puts sum_eq_n?([1,2,3,4,5], 9)
+puts sum_eq_n?([1,2,3,4,5], 1) # false
+puts sum_eq_n?([1,2,3,4,5], 6) # true
+puts sum_eq_n?([1,2,3,4,5], 13) # false
